2f595acaf2c10bcbfeafe6bf93c4ac558612ce96
sosinchon <115360056+sosinchon@users.noreply.github.com>
2022-10-08 19:02:52 +0300

    Add files via upload

---
+++ Souce.cpp
@@ -0,0 +1,57 @@
+﻿#include  <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "rus");
+	//1
+	/*int hp;
+	cin >> hp;
+	if (hp > 0) {
+		cout << "true" << endl;
+	}
+	if (hp < 0) {
+		cout << ("false") << endl;
+	}*/
+	//2
+	/*int t;
+	cin >> t;
+	if (t > 20) {
+		cout << ("вкл") << endl;
+	}
+		if (t < 20) {
+			cout << ("выкл") << endl;
+		}*/
+		//3
+	 /*int t, c, f;
+		cin >> t >> c >> f;
+		if (t > c && f <= 80) {
+			cout << "on" << endl;
+		}
+		else if (c <= t || f > 80) {
+			cout << "oof" << endl;
+		}*/
+		//4
+/* double x , y ;
+	cin >> x;
+	if (x > 0) {
+		 y = sqrt(x - 2);
+		cout << y << endl;
+	}
+	else if (x == 0) {
+		 y = 0;
+		cout << y << endl;
+	}
+	if (x < 0) {
+		 y = abc(x);
+		cout << y << endl;
+	}*/
+	//5
+	/*int finger;
+	cin >> finger;
+	switch (finger); {
+	case 1: cout << "большой палец" << endl; break;
+	case 2: cout << "средний палец" << endl; break;
+	case 3: cout << "средний палец" << endl; break;
+	case 4: cout << "безымянный палец" << endl; break;
+	case 5: cout << "мезинец " << endl; break;
+	default: cout << "ошибка" << endl; break;
+	}*/
